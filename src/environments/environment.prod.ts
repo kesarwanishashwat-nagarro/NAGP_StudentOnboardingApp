9ec078ea59fc850237f8f21635fc66560eec0945
Shashwat Kesarwani <shashwat.kesarwani@nagarro.com>
2019-04-14 23:41:50 +0530

    image resized and api url added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl : 'https://kesarwanishashwat-nagarro.github.io/NAGP_StudentOnboardingApp/'
+  apiUrl : 'https://my-json-server.typicode.com/kesarwanishashwat-nagarro/NAGP_StudentOnboardingApp'
 };
