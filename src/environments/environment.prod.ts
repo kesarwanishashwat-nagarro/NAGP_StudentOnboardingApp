84f1478e5387f74e6dd133e6a317f134e8f8bd30
Shashwat Kesarwani <shashwat.kesarwani@nagarro.com>
2019-04-14 23:44:17 +0530

    api url changed for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl : 'https://my-json-server.typicode.com/kesarwanishashwat-nagarro/NAGP_StudentOnboardingApp'
+  apiUrl : 'https://my-json-server.typicode.com/kesarwanishashwat-nagarro/NAGP_StudentOnboardingApp/'
 };
