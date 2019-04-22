062a56a13e04227d3a0f22c341fe0f05363d9967
Shashwat Kesarwani <shashwat.kesarwani@nagarro.com>
2019-04-22 14:16:38 +0530

    interceptor added

---
+++ src/app/core/api.interceptor.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+
+@Injectable()
+export class ApiInterceptor implements HttpInterceptor {
+
+  constructor() { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (!req.headers.has('Content-Type')) {
+      req = req.clone({ headers: req.headers.append('Content-Type', 'application/json') });
+    }
+    return next.handle(req);
+  }
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LoaderComponent } from './loader/loader.component';
 import { HeaderComponent } from './header/header.component';
 import { BsDropdownModule } from 'ngx-bootstrap';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { SharedModule } from '../shared/shared.module';
-
+import { ApiInterceptor } from './api.interceptor';
 @NgModule({
   declarations: [LoaderComponent, HeaderComponent, FooterComponent],
   imports: [
@@ -19,6 +19,14 @@ import { SharedModule } from '../shared/shared.module';
   ],
   exports: [
     LoaderComponent, HeaderComponent, FooterComponent
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ApiInterceptor,
+      multi: true
+    },
+    ApiInterceptor,
   ]
 })
 export class CoreModule { }
